b417d44590576a024db99aa54c99c471c786fcb6
chutima puthachon <chutima.p8@gmail.com>
2024-11-03 12:35:06 +1100

    added active menu

--- javascript/script.js
+++ javascript/script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
           contactMessage.textContent = 'Message sent successfully ✅';
           setTimeout(() => {
               contactMessage.textContent = '';
-          }, 3000);
+          }, 1500);
           contactForm.reset();
       }, (error) => {
           console.error('EmailJS error:', error);
@@ -20,3 +20,32 @@ document.addEventListener('DOMContentLoaded', function () {
 
   contactForm.addEventListener('submit', sendEmail);
 });
+
+// ROTATING RESUME DOWNLOAD
+const resume = "DOWNLOAD MY RESUME";
+const text = document.getElementById("circle_text");
+
+window.onload = () => {
+  for (let i = 0; i < resume.length; i++) {
+    let span = document.createElement('span');
+    span.innerHTML = resume[i];
+    text.appendChild(span);
+
+    span.style.transform = `rotate(${19 * i}deg)`;
+  }
+};
+
+// Get the container element
+var btnContainer = document.getElementsByClassName("nav_menu")[0];
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("nav_menu_item");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
